38710836f3b95edfeee47bab14fbef4d48978540
AnderssonHadad <tareasonlinemundial@gmail.com>
2026-02-28 18:10:02 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/")
+def home():
+    files = os.listdir(UPLOAD_FOLDER)
+    return render_template("index.html", files=files)
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["file"]
+    if file:
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    return "Archivo subido correctamente <a href='/'>Volver</a>"
+
+@app.route("/download/<filename>")
+def download(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
